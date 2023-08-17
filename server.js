81b3208cac66c8eeefc18e4ea0802149f7bf04ce
Prince <ubadinekeprince@gmaul.comgit config --global user.email ubadinekeprince@gmail.com>
2023-08-17 22:57:54 +0100

    Database Set Up

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
+
 const app = require('./app');
 
-const port = 3000;
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con => {
+    console.log('DB connection successful!')
+})
+
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 });
+
